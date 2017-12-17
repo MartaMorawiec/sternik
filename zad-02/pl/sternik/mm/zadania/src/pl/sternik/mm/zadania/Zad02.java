29a5992f4ba933b6c72278f2b4b81889a485f2f3
Marta Morawiec <marta.morawiec@edu.uekat.pl>
2017-12-17 10:04:13 +0100

    zadania

---
+++ zad-02/pl/sternik/mm/zadania/src/pl/sternik/mm/zadania/Zad02.java
@@ -0,0 +1,8 @@
+package pl.sternik.mm.zadania;
+
+public class Zad02 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Linia nr 2!");
+    }
+}
